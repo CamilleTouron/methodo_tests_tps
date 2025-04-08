ea34f77ac07bca96286b30676eb18e2a719fc106
camilletouron <camille.touron.pro@gmail.com>
2025-04-08 16:23:36 +0200

    TP3 - Bowling service et tests

--- tp_3/bowling/src/bowling/bowling.service.spec.ts
+++ tp_3/bowling/src/bowling/bowling.service.spec.ts
@@ -15,4 +15,48 @@ describe('BowlingService', () => {
   it('should be defined', () => {
     expect(service).toBeDefined();
   });
+
+    it('should return 300 for all strikes', () => {
+        for (let i = 0; i < 12; i++) {
+        service.roll(10);
+        }
+        expect(service.score()).toBe(300);
+    });
+
+    it('should return 150 for all spares', () => {
+        for (let i = 0; i < 21; i++) {
+            service.roll(5);
+        }
+        expect(service.score()).toBe(150);
+    });
+
+    it('should return 0 for all gutter balls', () => {
+        for (let i = 0; i < 20; i++) {
+            service.roll(0);
+        }
+        expect(service.score()).toBe(0);
+    });
+
+  it('should return 133 for mixed rolls', () => {
+    const service = new BowlingService();
+    service.roll(10);
+    service.roll(7);
+    service.roll(3);
+    service.roll(9);
+    service.roll(0);
+    service.roll(10);
+    service.roll(0);
+    service.roll(8);
+    service.roll(8);
+    service.roll(2);
+    service.roll(10);
+    service.roll(9);
+    service.roll(1);
+    service.roll(10);
+    service.roll(10);
+    service.roll(10);
+    service.roll(3);
+
+    expect(service.score()).toBe(187);
+  });
 });
